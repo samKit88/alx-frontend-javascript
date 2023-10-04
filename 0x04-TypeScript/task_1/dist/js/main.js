838181de922ed3130e3f72b55c2ccc316b82c8c7
samKit88 <samuel.girma.cx9896@gmail.com>
2023-10-04 05:32:28 -0800

    ALX Typescript

---
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,15 @@
+var printTeacher = function (firstName, lastName) { return "".concat(firstName.charAt(0), ". ").concat(lastName); };
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        var _this = this;
+        this.workOnHomework = function () { return 'Currently working'; };
+        this.displayName = function () { return _this.firstName; };
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    return StudentClass;
+}());
+var student = new StudentClass("Paul", "jerry");
+console.log(student.displayName());
+console.log(student.workOnHomework());
+//# sourceMappingURL=main.js.map
